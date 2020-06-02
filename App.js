8587948d27ecc1d3dd42fed7c546b99f722376fc
Bryce DesBrisay <bryce@desbrisay.com>
2020-06-02 01:34:31 -0700

    updated navigator

--- App.js
+++ App.js
@@ -1,61 +1,67 @@
 import React from 'react';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import Home from './components/Home';
 import SignIn from './components/SignIn';
 
-import { BottomNavigation } from 'react-native-paper';
-
-class App extends React.Component {
-  state = {
-    index: 0,
-    routes: [
-      { key: 'home', title: 'Home', icon: 'home', color: '#32a875' },
-      { key: 'signin', title: 'Sign In', icon: 'account_circle', color: '#4e32a8' }
-    ],
-  };
-
-  _handleIndexChange = index => this.setState({ index });
-
-  _renderScene = BottomNavigation.SceneMap({
-    home: Home,
-    signin: SignIn,
-  });
-
-  render() {
-    return (
-      <BottomNavigation
-        navigationState={this.state}
-        onIndexChange={this._handleIndexChange}
-        renderScene={this._renderScene}
-        shifting={true}
-        labeled={true}
-      />
-    );
-  }
-}
-
-/*
+const Stack = createStackNavigator();
 const Tab = createMaterialBottomTabNavigator();
+
+function TabsNav() {
+  return (
+    <Tab.Navigator
+      style={{ backgroundColor: 'tomato' }}
+      backgroundColor="green"
+      shifting={false}
+    >
+      <Tab.Screen 
+        name="Home" 
+        component={Home} 
+        options={{
+          tabBarColor: '#157efb',
+          tabBarIcon: ({ color }) => <Icon name="home" size={24} color={color} />
+        }}
+      />
+      <Tab.Screen 
+        name="account" 
+        component={SignIn} 
+        options={{
+          tabBarColor: '#0471f2',
+          tabBarIcon: ({ color }) => <Icon name="account" size={24} color={color} />
+        }}
+      />
+      <Tab.Screen 
+        name="Pay Online" 
+        component={SignIn} 
+        options={{
+          tabBarColor: '#0471f2',
+          tabBarIcon: ({ color }) => <Icon name="credit-card" size={24} color={color} />
+        }}
+      />
+      <Tab.Screen 
+        name="Usage" 
+        component={SignIn} 
+        options={{
+          tabBarColor: '#0471f2',
+          tabBarIcon: ({ color }) => <Icon name="gauge" size={24} color={color} />
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen 
-          name="Home" 
-          component={Home} 
-          options={{
-            tabBarIcon: () => <Icon name="home" />
-          }}
-        />
-        <Tab.Screen name="SignIn" component={SignIn} />
-      </Tab.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Tabs" component={TabsNav} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
-*/
 
 export default App;
